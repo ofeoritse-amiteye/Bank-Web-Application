151de11f887c550803d2054f5aaf00a8e9db89a2
ofeoritse Amiteye <ofeoritseamiteye03@gmail.com>
2025-02-20 23:52:20 +0100

    New Changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,7 +40,7 @@ const Dashboard: React.FC = () => {
   return (
     <div className="flex flex-col h-full">
 
-        <div className=' w-full shadow-sm flex sticky z-1000 top-0 bg-white'>
+      <div className=' w-full shadow-sm flex sticky z-1000 top-0 bg-white h-24'>
 
         <div className="custom:hidden p-4">
           <button onClick={handleSidebarToggle} className="text-[#213F7D]">
@@ -50,12 +50,12 @@ const Dashboard: React.FC = () => {
           </button>
          </div>
             <div className='flex justify-center items-center'>
-                <div className='p-10 hidden custom:block '>
-                <Image src={"/images/logo.png"} alt="no image" height={100} width={173.76}/>
+                <div className='hidden custom:block '>
+                <Image src={"/images/inside-out.png"} alt="no image" height={100} width={173.76}/>
                 </div>
             </div>
             <div className='w-1/2 flex items-center'>
-            <Image src={"/images/logo.png"} alt='no image' height={30} width={150} className='md:hidden'/>
+            <Image src={"/images/inside-out.png"} alt='no image' height={30} width={150} className='md:hidden'/>
             <input placeholder='search for anything' type="search" name="" id="" className='p-2 h-12 w-2/3 ml-16 rounded-l-lg border-2 border-gray-300 hidden md:block'/>
             <button type="submit" className='bg-[#39CDCC] h-12 w-14 rounded-r-lg items-center justify-center md:flex hidden'>
                 <Image src={"/icons/search.png"} alt='no image' height={25} width={25}/>
@@ -77,21 +77,21 @@ const Dashboard: React.FC = () => {
             <div className='flex'>
                 <div className="w-full hidden lg:block">
                     <Image
-                        src="/icons/avatar.png"
+                        src="/images/ofe.png"
                         alt=""
                         width={112}
                         height={112}
                         className="w-10 h-10 rounded-full object-cover"
                     />
                 </div>
-                <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" className=" text-[#213F7D] font-medium text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Adeleji 
+                <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" className=" text-[#213F7D] font-medium text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Ofeoritse 
                     <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
                     </svg>
                 </button>
             </div>
           </div>
-        </div>
+      </div>
         
         <div className='flex flex-grow h-full'>
           <div className='hidden custom:block w-[283px] h-full overflow-y-auto'>
