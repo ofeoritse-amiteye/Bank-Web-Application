8042eb7c164cffeb901743026734e683221885a9
ofeoritse Amiteye <ofeoritseamiteye03@gmail.com>
2025-03-14 22:32:05 +0100

    FIx :made updates to the logo

--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -44,7 +44,7 @@ export default function Loginform() {
             password: 12345USER
           </div>
         )}
-      <Image src={"/images/logo.png"} alt="no image" height={100} width={173.76} className="md:hidden mb-10"/>
+                        <h1 className="text-black text-3xl font-bold flex md:hidden mb-10">Walk <span className="text-red-400">Man</span></h1>
         <h2 className="text-[40px] font-bold text-[#213F7D]">Welcome!</h2>
         <p className="text-[#545F7D]">Enter details to Login</p>
         <div className="mt-10 mb-5 w-full">
